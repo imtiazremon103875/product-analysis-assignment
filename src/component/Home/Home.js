dee32e26302c20715f847bd837101de8af5c50a8
imtiaz remon <imtiazremon10375@gmail.com>
2022-04-04 02:08:15 +0600

    home compltet

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,8 +3,16 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is home</h1>
+        <div className='home-container'>
+            <div className="home-details">
+                <h1 className='headline'>Movie lovers Bangladesh</h1>
+                <h2>Your Favourite Movie list</h2>
+                <p className='home-para'>Three Idiots’ is a remarkable ahead of its time Bollywood blockbuster. This film is a comedy movie with strong acting, memorable characters, a perplexing storyline and most importantly, highly motivational movie to choose the right path in your life.The producers of the movie expressed some amazing scenery of the mountainous landscape of Ladakh. These scenes were taken with various different camera angles such as high angle, crane and parallax shots</p>
+            </div>
+            <div className="image-container">
+                <img src="https://upload.wikimedia.org/wikipedia/en/d/df/3_idiots_poster.jpg" alt="" />
+            </div>
+
         </div>
     );
 };
