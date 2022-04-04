fc00715e9f6e542b19e2d645c08b5a6a651a7ba8
imtiaz remon <imtiazremon10375@gmail.com>
2022-04-04 14:43:37 +0600

    Blogs section done

--- src/component/Blogs/Blogs.js
+++ src/component/Blogs/Blogs.js
@@ -4,7 +4,12 @@ import './Blogs.css'
 const Blogs = () => {
     return (
         <div>
-            <h2>this is blogs</h2>
+            <h3>What is context api</h3>
+            <p>The React Context API is a manner for a React app to correctly produce worldwide variables that may be exceeded around. This is the opportunity to "prop drilling" or shifting props from grandparent to baby to parent, and so on. Context is likewise touted as an easier, lighter technique to nation control the use of Redux.The Context API is a React shape that permits you to trade precise info and assists in fixing prop-drilling from all degrees of your applicatio</p>
+            <h3>what is sementic tag</h3>
+            <p>Semantic HTML or semantic markup is HTML that introduces which means to the net web page in preference to simply presentation. For example, a p  tag shows that the enclosed textual content is a paragraph. This is each semantic and presentational due to the fact human beings recognize what paragraphs are, and browsers recognize a way to show them.
+
+                On the turn facet of this equation, tags such as b and i tag  aren't semantic. They outline most effective how the textual content have to look (formidable or italic), and do not offer any extra which means to the markup.</p>
         </div>
     );
 };
